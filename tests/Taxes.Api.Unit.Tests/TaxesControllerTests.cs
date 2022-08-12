c5dfc03cc6d129e253f3e58e80b9a743cb05bb49
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-08-12 08:12:25 -0300

    tests: 200 status code when any items found

---
+++ tests/Taxes.Api.Unit.Tests/TaxesControllerTests.cs
@@ -0,0 +1,48 @@
+using System;
+
+using FluentAssertions;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+
+using Taxes.Api.Controllers;
+using Taxes.Api.Models;
+using Taxes.Api.Requests;
+using Taxes.Api.Responses;
+using Taxes.Api.Services;
+
+using Xunit;
+
+namespace Taxes.Api.Tests;
+
+public class TaxesControllerTests
+{
+    [Fact]
+    public async void WhenAnyItemsFoundMustReturn200StatusCode()
+    {
+        //arrange
+        var request = new TaxSearchRequest 
+        {
+            StartAt = DateTime.Now,
+            EndAt = DateTime.Now.AddDays(2) 
+        };
+
+        var mockService = new Mock<ITaxesSearchService>();
+
+        var taxes = new TaxSearchResponse
+        {
+          new Tax(request.StartAt.ToString("dd/MM/yyyy"), "5000", TaxType.Selic)
+        };
+
+        mockService.Setup(s => s.SearchByAsync(request)).ReturnsAsync(taxes);
+
+        var sut = new TaxesController();
+
+        //act 
+        var response = await sut.SearchSelicTaxAsync(request, mockService.Object);
+
+        //assert
+        response.Should().BeOfType<OkObjectResult>();
+    }
+}
